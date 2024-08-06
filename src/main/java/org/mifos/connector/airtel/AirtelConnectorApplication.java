3cee49e8add9594680276d0b924752d7c4753073
sanchit-0 <sanchit@fynarfin.io>
2024-08-06 10:25:49 +0530

    Create mock Airtel APIs. The response codes in the `application.yml` file have been added from the Airtel documentation.
    Create Airtel connector.
    Create zeebe workers for airtel connector.

--- src/main/java/org/mifos/connector/airtel/AirtelConnectorApplication.java
+++ src/main/java/org/mifos/connector/airtel/AirtelConnectorApplication.java
@@ -3,8 +3,10 @@ package org.mifos.connector.airtel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableCaching
+@EnableAsync
 @SpringBootApplication
 @SuppressWarnings({ "HideUtilityClassConstructor" })
 public class AirtelConnectorApplication {
